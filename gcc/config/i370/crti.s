50c7eaca2746e300acedada3a84bf1e3397b82b7
Linas Vepštas <linasvepstas@gmail.com>
2024-10-03 15:41:22 -0500

    Attempted hack of crti.s and crtn.s
    I'm not sure this is correct, and at any rate, its not currently
    used by anyone, so this is an academic exercise. Whatever. It was
    getting on my nerves, so I took a shot.

--- gcc/config/i370/crti.s
+++ gcc/config/i370/crti.s
@@ -1,17 +1,59 @@
-/* Bogus .init and .fini sections for i370-ibm-linux target.
-   Just enough to allow basic builds. */
+#
+# This file just supplies function prologues for the .init and .fini
+# sections.  It is linked in before crtbegin.o. It is supposed to
+# provide a standard function prolog; users can put "anything" in
+# here. _init is (supposed to be) called directly from _start
+# (crt0.s or crt1.s provided by the C library). The _fini is
+# called by atexit() from crt0.s/crt1.s.
+#
+# crti.s contains the prologues to these two sections, and crtn.s
+# contains the epilogs. STARTFILE_SPEC should list crti.o before
+# any other object files that might add code to .init or .fini
+# sections, and ENDFILE_SPEC should list crtn.o after any such object
+# files
+#
+# I have no clue if this is correct.
 
-	.file   "crti.o"
-	.ident  "GNU C crti.o"
+	.file   "crti.s"
+	.ident  "GNU C crti.s"
 
 	.section .init
 	.globl	_init
 	.type	_init,@function
 _init:
-	nopr	0
+	.using	.,r15
+	B	.Linit
+	.long	88	# stacksize
+	.long	_initpgtable
+	.drop	r15
+	.balign	2
+.Linit:
+	STM	r13,r12,8(r11)
+	LR	r13,r11
+	A	r11,4(,r15)
+	L	r4,8(,r15)
+	BASR	r3,0
+	.using	.,r3
+_initpage:
+	ST	r4,0(r13)
+
 
 	.section .fini
 	.globl	_fini
 	.type	_fini,@function
 _fini:
-	nopr	0
+	.using	.,r15
+	B	.Lfini
+	.long	88	# stacksize
+	.long	_finipgtable
+	.drop	r15
+	.balign	2
+.Lfini:
+	STM	r13,r12,8(r11)
+	LR	r13,r11
+	A	r11,4(,r15)
+	L	r4,8(,r15)
+	BASR	r3,0
+	.using	.,r3
+_finipage:
+	ST	r4,0(r13)
--- gcc/config/i370/crtn.s
+++ gcc/config/i370/crtn.s
@@ -1,11 +1,32 @@
-/* Bogus .init and .fini sections for i370-ibm-linux target.
-   Just enough to allow basic builds. */
+#
+# This file just makes sure that the .fini and .init sections do in
+# fact return.  Users may put any desired instructions in those sections.
+# This file is the last thing linked into any executable.
+# See crti.s for more info.
 
-	.file   "crtn.o"
-	.ident  "GNU C crtn.o"
+	.file   "crtn.s"
+	.ident  "GNU C crtn.s"
 
 	.section .init
-	nopr	0
+	L	r14,12(,r13)
+	LM	2,12,28(r13)
+	L	r13,8(,r13)
+	BASR	r1,r14
+# Function page table
+	.balign	4
+_initpgtable:
+	.long	_initpage
+# Uhhh
+#	.size _init, .-_init
 
 	.section .fini
-	nopr	0
+	L	r14,12(,r13)
+	LM	2,12,28(r13)
+	L	r13,8(,r13)
+	BASR	r1,r14
+# Function page table
+	.balign	4
+_finipgtable:
+	.long	_finipage
+# Uhhh
+#	.size _fini, .-_fini
